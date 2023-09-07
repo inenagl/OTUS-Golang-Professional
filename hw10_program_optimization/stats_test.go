42911918300d58ae9784feac7461b525c0463d20
inenaglyadkin <inenaglyadkin@rambler-co.ru>
2023-09-07 11:36:26 +0300

    HW10 is completed
    Benchmarks:
    cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
                     │   old.out    │               new.out               │
                     │    sec/op    │   sec/op     vs base                │
    GetDomainStat-12   623.5m ± 10%   170.5m ± 2%  -72.66% (p=0.000 n=10)
    
                     │    old.out    │               new.out                │
                     │     B/op      │     B/op      vs base                │
    GetDomainStat-12   308.80Mi ± 0%   20.50Mi ± 0%  -93.36% (p=0.000 n=10)
    
                     │   old.out    │               new.out               │
                     │  allocs/op   │  allocs/op   vs base                │
    GetDomainStat-12   3145.4k ± 0%   212.1k ± 0%  -93.26% (p=0.000 n=10)

--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -1,9 +1,12 @@
+//go:build !bench
 // +build !bench
 
 package hw10programoptimization
 
 import (
+	"archive/zip"
 	"bytes"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -36,4 +39,64 @@ func TestGetDomainStat(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, DomainStat{}, result)
 	})
+}
+
+func TestGetDomainStatBorderConditions(t *testing.T) {
+	data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
+	t.Run("find empty", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(data), "")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{}, result)
+	})
+
+	data = `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}`
+	t.Run("find when there is no email field in data", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{}, result)
+	})
+
+	data = `
+
+`
+	t.Run("find in empty multiline data", func(t *testing.T) {
+		_, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.Error(t, err)
+		require.Equal(t, "error: invalid json: ''", err.Error())
+	})
+
+	data = `{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com",Phone:"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}`
+	t.Run("find in invalid json", func(t *testing.T) {
+		_, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.Error(t, err)
+		require.Equal(t, fmt.Sprintf("error: invalid json: '%s'", data), err.Error())
+	})
+}
+
+func BenchmarkGetDomainStat(b *testing.B) {
+	b.StopTimer()
+
+	r, err := zip.OpenReader("testdata/users.dat.zip")
+	require.NoError(b, err)
+	defer r.Close()
+
+	require.Equal(b, 1, len(r.File))
+
+	for i := 0; i < b.N; i++ {
+		data, err := r.File[0].Open()
+		require.NoError(b, err)
+
+		b.StartTimer()
+		_, err = GetDomainStat(data, "biz")
+		b.StopTimer()
+		require.NoError(b, err)
+
+		err = data.Close()
+		require.NoError(b, err)
+	}
 }
